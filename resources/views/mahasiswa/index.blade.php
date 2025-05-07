fc116ad4bb91f2578290a3c512e6c7c5cf53e5a6
sherlynclau <sherlynclaurensia_2327240134@mhs.mdp.ac.id>
2025-05-07 15:03:40 +0700

    adminlte

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,27 +1,69 @@
 @extends('layout.main')
+@section('title', 'Mahasiswa')
 
 @section('content')
-    <h1>Mahasiswa</h1>
-    <table>
-        <tr>    
-            <th>NPM</th>
-            <th>Nama</th>
-            <th>JK</th>
-            <th>Tanggal lahir</th>
-            <th>Tempat lahir</th>
-            <th>Asal SMA</th>
-            <th>Prodi</th>
-        </tr>
-    @foreach ($mahasiswa as $item)
-        <tr>
-            <td>{{ $item->npm }}</td>
-            <td>{{ $item->nama }}</td>
-            <td>{{ $item->jk }}</td>
-            <td>{{ $item->tanggal_lahir }}</td>
-            <td>{{ $item->tempat_lahir }}</td>
-            <td>{{ $item->asal_sma }}</td>
-            <td>{{ $item->prodi->nama }}</td>
-        </tr>
-    @endforeach
-    </table>
+     <!--begin::Row-->
+     <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">List Mahasiswa</h3>
+              <div class="card-tools">
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-collapse"
+                  title="Collapse"
+                >
+                  <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                  <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-remove"
+                  title="Remove"
+                >
+                  <i class="bi bi-x-lg"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card-body">
+                <table class = "table">
+                    <thead>
+                        <tr>    
+                            <th>NPM</th>
+                            <th>Nama</th>
+                            <th>JK</th>
+                            <th>Tanggal lahir</th>
+                            <th>Tempat lahir</th>
+                            <th>Asal SMA</th>
+                            <th>Prodi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($mahasiswa as $item)
+                            <tr>
+                                <td>{{ $item->npm }}</td>
+                                <td>{{ $item->nama }}</td>
+                                <td>{{ $item->jk }}</td>
+                                <td>{{ $item->tanggal_lahir }}</td>
+                                <td>{{ $item->tempat_lahir }}</td>
+                                <td>{{ $item->asal_sma }}</td>
+                                <td>{{ $item->prodi->nama }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+        </div>
+            <!-- /.card-body -->
+            {{-- <div class="card-footer">Footer</div> --}}
+            <!-- /.card-footer-->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+      <!--end::Row-->
+    
 @endsection
--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,25 +1,65 @@
 @extends('layout.main')
-
+@section('title', 'Program Studi')
+    
 @section('content')
-    <h1>Program Studi</h1>
-
-    <table>
-        <tr>
-            <th>Nama</th>
-            <th>Singkatan</th>
-            <th>Kaprodi</th>
-            <th>Sekretaris</th>
-            <th>Fakultas</th>
-        </tr>
-
-    @foreach ($prodi as $item)
-        <tr>
-            <td>{{$item->nama}}</td>
-            <td>{{$item->singkatan}}</td>
-            <td>{{$item->kaprodi}}</td>
-            <td>{{$item->sekretaris}}</td>
-            <td>{{$item->fakultas->nama}}</td>
-        </tr>
-    @endforeach 
-    </table>
+    <!--begin::Row-->
+    <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">List Program Studi</h3>
+              <div class="card-tools">
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-collapse"
+                  title="Collapse"
+                >
+                  <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                  <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-remove"
+                  title="Remove"
+                >
+                  <i class="bi bi-x-lg"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card-body">
+                <table class = "table">
+                    <thead>
+                        <tr>
+                            <th>Nama</th>
+                            <th>Singkatan</th>
+                            <th>Kaprodi</th>
+                            <th>Sekretaris</th>
+                            <th>Fakultas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($prodi as $item)
+                            <tr>
+                                <td>{{$item->nama}}</td>
+                                <td>{{$item->singkatan}}</td>
+                                <td>{{$item->kaprodi}}</td>
+                                <td>{{$item->sekretaris}}</td>
+                                <td>{{$item->fakultas->nama}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+        </div>
+            <!-- /.card-body -->
+            {{-- <div class="card-footer">Footer</div> --}}
+            <!-- /.card-footer-->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+      <!--end::Row-->
+    
 @endsection
