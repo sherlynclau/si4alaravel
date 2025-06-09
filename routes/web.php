960d6978260e95e1fb18206a4a2bdc03eb526e40
sherlynclau <sherlynclaurensia_2327240134@mhs.mdp.ac.id>
2025-06-09 15:19:47 +0700

    web php

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,36 @@
-    <?php
+<?php
 
-    use App\Http\Controllers\DashboardController;
-    use App\Http\Controllers\FakultasController;
-    use App\Http\Controllers\JadwalController;
-    use App\Http\Controllers\MahasiswaController;
-    use App\Http\Controllers\MatakuliahController;
-    use App\Http\Controllers\ProdiController;
-    use App\Http\Controllers\ProfileController;
-    use App\Http\Controllers\SesiController;
-    use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\SesiController;
+use App\Http\Controllers\MataKuliahController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use Illuminate\Support\Facades\Route;
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-    require __DIR__.'/auth.php';
+Route::resource('/fakultas', FakultasController::class);
+Route::resource('/prodi', ProdiController::class); // menambahkan route resource prodi
+Route::resource('/mahasiswa', MahasiswaController::class); // menambahkan route resource mahasiswa
+Route::resource('/sesi', SesiController::class); // menambahkan route resource sesi
+Route::resource('/matakuliah', MataKuliahController::class); // menambahkan route resource mata kuliah
+Route::resource('/jadwal', JadwalController::class); // menambahkan route resource jadwal
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard'); // menambahkan route dashboard
 
-    Route::resource('/fakultas', FakultasController::class);
-    Route::resource('/prodi', ProdiController::class);  
-    Route::resource('/mahasiswa', MahasiswaController::class);
-    Route::resource('/matakuliah', MatakuliahController::class);
-    Route::resource('/jadwal', JadwalController::class);
-    Route::resource('/sesi', SesiController::class);
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard'); 
+Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
+require __DIR__.'/auth.php';
